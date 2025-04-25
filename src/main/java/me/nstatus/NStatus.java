214fb45ce60bc3ec0a797e8284dfba74712cb362
KillDarkness <killdarkness45@gmail.com>
2025-04-25 14:18:12 -0300

    🎲 Mudanças simples

--- src/main/java/me/nstatus/NStatus.java
+++ src/main/java/me/nstatus/NStatus.java
@@ -2,34 +2,36 @@ package me.nstatus;
 
 import me.nstatus.routes.Routes;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import static spark.Spark.*;
 
 public class NStatus extends JavaPlugin {
 
+    // ANSI azul
+    private static final String AZUL = "\u001B[34m";
+    private static final String RESET = "\u001B[0m";
+
     @Override
     public void onEnable() {
-        // Salva a configuração padrão se não existir
         saveDefaultConfig();
-        
-        // Verifica se a porta está configurada
+
         if (!getConfig().contains("http-port") || getConfig().getString("http-port").isEmpty()) {
             getLogger().severe("Porta HTTP não configurada no config.yml!");
             getLogger().severe("O plugin nStatus continuará funcionando, mas o servidor HTTP não será iniciado.");
             return;
         }
-        
+
         try {
-            // Obtém a porta do arquivo de configuração
             int httpPort = getConfig().getInt("http-port");
-            
-            // Configura a porta HTTP
             port(httpPort);
-            
-            // Inicializa as rotas
+
             Routes routes = new Routes(this);
             routes.setupRoutes();
-            
-            getLogger().info("nStatus 1.21 rodando em http://[IP]:" + httpPort);
+
+            // Prints em azul com prefixo customizado
+            System.out.println(AZUL + "[nStatus] nStatus 1.21 iniciado com sucesso!" + RESET);
+            System.out.println(AZUL + "[nStatus] Documentação do plugin: http://[IP]:" + httpPort + "/doc" + RESET);
+
         } catch (Exception e) {
             getLogger().severe("Erro ao iniciar o servidor HTTP: " + e.getMessage());
             getLogger().severe("O plugin nStatus continuará funcionando, mas o servidor HTTP não será iniciado.");
@@ -39,7 +41,7 @@ public class NStatus extends JavaPlugin {
     @Override
     public void onDisable() {
         try {
-            stop(); // Encerra o servidor HTTP
+            stop();
             getLogger().info("Servidor HTTP do nStatus encerrado.");
         } catch (Exception e) {
             getLogger().info("O servidor HTTP já estava parado ou ocorreu um erro ao encerrá-lo.");
